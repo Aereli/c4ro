bbfcc6098de474f35ec3f58899dee2479270c997
Eric Oyanadel <eric.oyanadel@slalom.com>
2022-02-18 12:58:27 -0500

    update configuration for m1 mac

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,11 +3,13 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-import path from "path"
-
-// Splinters
-async function splinterPages({ graphql, actions }) {
+const path = require(`path`)
+//  // Log out information after a build is done
+//  exports.onPostBuild = ({ reporter }) => {
+//    reporter.info(`Your Gatsby site has been built!`)
+//  }
+//  // Create blog pages dynamically
+exports.createPages = async ({ graphql, actions }) => {
   const { data } = await graphql(`
     {
       allProjectsJson {
@@ -29,6 +31,34 @@ async function splinterPages({ graphql, actions }) {
   })
 }
 
-export async function createPages(params) {
-  await Promise.all([splinterPages(params)])
-}
+// THIS WAS NOT WORKING ON THE M1 MAC
+// MUST BE REVISED IF YOU WANT TO USE ES MODULES
+
+// import path from "path"
+
+// // Splinters
+// async function splinterPages({ graphql, actions }) {
+//   const { data } = await graphql(`
+//     {
+//       allProjectsJson {
+//         nodes {
+//           slug
+//         }
+//       }
+//     }
+//   `)
+//   // console.log("nodesss:", data.allHomepageJson.edges[0].node)
+//   data.allProjectsJson.nodes.forEach(project => {
+//     actions.createPage({
+//       path: `/project/${project.slug}`,
+//       component: path.resolve("./src/templates/projects-template.js"),
+//       context: {
+//         slug: project.slug,
+//       },
+//     })
+//   })
+// }
+
+// export async function createPages(params) {
+//   await Promise.all([splinterPages(params)])
+// }
