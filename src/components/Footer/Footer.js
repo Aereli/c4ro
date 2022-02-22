49b7ecf44c8876d134f056f500309f188557617e
Eric Oyanadel <eric.oyanadel@slalom.com>
2022-02-22 11:38:14 -0500

    update home page footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,26 @@
 import React from "react"
 import styles from "./styles.module.scss"
+import email from '../../images/icon-email.png'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h2>Carolina Rivera</h2>
-        <p>"Miami Based Designer</p>
+        <div className={styles.links}>
+             <img src={email} alt="email icon" />
+          <p>
+            <a href="mailto:carolinariveradesign@gmail.com">
+              CarolinaRiveraDesign@gmail.com
+            </a>
+          </p>
+        </div>
+        <div className={styles.description}>
+          <h2>Carolina Rivera</h2>
+          <h3>"Miami Based Designer"</h3>
+          <p>
+            Background in architecture with professional experience in interiors
+          </p>
+        </div>
       </div>
     </div>
   )
