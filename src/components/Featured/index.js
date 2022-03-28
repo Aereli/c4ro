22d6f0b00dd2f096b57123fdfa5525162f05551d
Eric Oyanadel <eric.oyanadel@slalom.com>
2022-03-28 14:14:30 -0400

    add featured component to home

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -1,25 +1,70 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import GetFeaturedData from "../../utils/getFeaturedData"
 import styles from "./styles.module.scss"
+import Lightbox from "react-image-lightbox"
+import "react-image-lightbox/style.css"
 
 const Featured = () => {
   const data = GetFeaturedData()
 
+  const [photoIndex, setPhotoIndex] = useState(0)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const allImages = data && data.allFeatured.map(item => item.image.asset.url)
+
+  useEffect(() => {
+    const scrollContainer = document.querySelector("#test")
+    if (data) {
+      scrollContainer.addEventListener("wheel", evt => {
+        evt.preventDefault()
+        scrollContainer.scrollLeft += evt.deltaY
+      })
+    }
+  }, [data])
+
+  function imageHandler(item) {
+    setPhotoIndex(item - 1)
+    setIsOpen(true)
+  }
+
   return (
     <div className={styles.container}>
-      {!data ? 
+      <h1>Featured</h1>
+      {!data ? (
         <p>Loading</p>
-       : 
-        <div>
-          <h1>Featured</h1>
-          {data.allFeatured.map(item => (
+      ) : (
+        <div id="test" className={styles.imagesContainer}>
+          {data.allFeatured.map((item, id) => (
             <div key={item._id}>
-              <p>{item.title}</p>
-              <img src={item.image.asset.url} alt={item.title} />
+              {/* <p>{item.title}</p> */}
+              <img
+                src={item.image.asset.url}
+                alt={item.title}
+                onClick={() => imageHandler(id)}
+              />
             </div>
           ))}
         </div>
-      }
+      )}
+
+      {isOpen && (
+        <Lightbox
+          mainSrc={allImages[photoIndex]}
+          nextSrc={allImages[(photoIndex + 1) % allImages.length]}
+          prevSrc={
+            allImages[(photoIndex + allImages.length - 1) % allImages.length]
+          }
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setPhotoIndex(
+              (photoIndex + allImages.length - 1) % allImages.length
+            )
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % allImages.length)
+          }
+        ></Lightbox>
+      )}
     </div>
   )
 }
